5534f22cc6bcccdb1cc47cb7074b166fe1cd32c6
Vladislav_Poroshin <vld.poroshin@gmail.com>
2024-01-26 14:44:49 +0400

    Basic models created

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Ingredient(models.Model):
+    """Ingredient модель."""
+
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='Название продукта'
+    )
+    count_use = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Счетчик использований'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ['name']
+
+
+class Recipe(models.Model):
+    """Recipe модель"""
+
+    name = models.CharField(max_length=100, verbose_name='Название рецепта')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['name']
+
+
+class RecipeIngredient(models.Model):
+    """Модель веса ингредиентов"""
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.SET_NULL,
+        verbose_name='Ингредиент',
+        **NULLABLE
+    )
+    quantity = models.PositiveIntegerField(verbose_name='Вес в гр.')
+
+    def __str__(self):
+        return f'{self.ingredient} - {self.quantity}'
+
+    class Meta:
+        verbose_name = 'Вес ингредиента для рецепта'
+        verbose_name_plural = 'Вес ингредиентов для рецепта'
