dd2324af6d968aa46fb4bbd73ad7e154a408d9d8
Vladislav_Poroshin <vld.poroshin@gmail.com>
2024-01-27 05:44:24 +0400

    fix code

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 
 from recipe.apps import RecipeConfig
-from recipe.views import add_product_to_recipe, cook_recipe, IngredientListView
+from recipe.views import (add_product_to_recipe, cook_recipe,
+                          IngredientListView)
 
 app_name = RecipeConfig.name
 
 urlpatterns = [
-    path('product/add/', add_product_to_recipe, name='add_product_to_recipe'),
-    path('cook/', cook_recipe, name='cook_recipe'),
-    path('<int:pk>/show/', IngredientListView.as_view(), name='show'),
+    path('product/add/',
+         add_product_to_recipe,
+         name='add_product_to_recipe'
+         ),
+    path('cook/',
+         cook_recipe,
+         name='cook_recipe'
+         ),
+    path('<int:pk>/show/',
+         IngredientListView.as_view(),
+         name='show_recipes_without_product'
+         ),
 ]
